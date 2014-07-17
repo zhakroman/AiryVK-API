8fb1efde73999c351ec46a938f99b29a07700268
Roman Zhak <zhak55@ya.ru>
2014-07-17 12:27:57 +0400

    Update case.js

--- Init/case.js
+++ Init/case.js
@@ -5,13 +5,12 @@ VK.init(function() {
   // New instance of AiryVK API will be created 
   var req = new vk();
 
-
-
  }, function() { 
      // API initialization failed 
      // Can reload page here
     vk.again() ;
     // vk.latest is the latest version of VK API
 }, vk.latest ); 
+
 
 
