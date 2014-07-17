145a019e6e87e3abbf32d1bf4052aec7451f981e
Roman Zhak <zhak55@ya.ru>
2014-07-17 12:58:07 +0400

    Update case.js

---
+++ FlashVars/case.js
@@ -0,0 +1,27 @@
+ 
+ vk.latest // returns the latest version of VK API
+ vk.id     // returns the id of current user ( alternative is viewer_id  )
+ 
+ // Other methods are default 
+ 
+ vk.api_url // e.g.: http://api.vk.com/api.php 
+ vk.api_id  
+ vk.api_settings
+ vk.viewer_type
+ vk.sid
+ vk.secret
+ vk.access_token
+ vk.group_id
+ vk.is_app_user
+ vk.auth_key
+ vk.language 
+ vk.parent_language
+ vk.ad_info
+ vk.is_secure
+ vk.ads_app_id
+ vk.referrer 
+ vk.lc_name
+ vk.hash
+ vk.user_id 
+ 
+ // details you can find out here: http://xaa.su/RGp
